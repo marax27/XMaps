783466d91694c76352b0fb1fb894f5a6e2e22fe7
Kacper Tonia <32137911+marax27@users.noreply.github.com>
2023-02-22 02:19:19 +0100

    Custom attribute tests (#1)
    * Allow inheriting from AtAttribute
    
    * Update AtAttribute's docstring
    
    * Add sad path test

--- src/XMaps/AtAttribute.cs
+++ src/XMaps/AtAttribute.cs
@@ -3,13 +3,30 @@
 namespace XMaps;
 
 /// <summary>
-/// Responsible for mapping parts of HTML into a model.
+/// It is an attribute that allows you to annotate a model and its constructor parameters with 
+/// XPath expressions. This way, the mapper will know which HTML nodes to use when initializing the model.<br/>
 /// The <c>AtAttribute</c> can be applied in 2 ways:
-/// 1. Applied to a class, in order to define mapping's entry point.
-/// 2. Applied to a constructor parameter in order to map node(s) to this parameter.
+/// <list type="bullet">
+///     <item>
+///         <term>to a model type</term>
+///         <description>
+///             optional. When applied to a model type, the attribute's XPath expression
+///             serves as a starting point for the model's constructor parameters.
+///         </description>
+///     </item>
+///     <item>
+///         <term>to a constructor parameter</term>
+///         <description>
+///             mandatory. It defines which HTML node corresponds to the parameter.
+///         </description>
+///     </item>
+/// </list>
 /// </summary>
+/// <remarks>
+/// You can create a custom attribute that inherits from <c>AtAttribute</c>.
+/// </remarks>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Parameter, AllowMultiple = false)]
-public sealed class AtAttribute : Attribute
+public class AtAttribute : Attribute
 {
     public string XPath { get; }
 
---
+++ tests/XMaps.UnitTests/CustomAtAttribute/HappyPath/SearchByClassAttributeTests.cs
@@ -0,0 +1,41 @@
+﻿namespace XMaps.UnitTests.CustomAtAttribute.HappyPath;
+
+public class SearchByClassAttributeTests
+{
+    private const string GivenHtml = """
+    <html>
+        <body>
+            <h1>Welcome</h1>
+            <div class="main">
+                <p>Lorem ipsum</p>
+            </div>
+            <div class="footer">
+                <span>Bottom line</span>
+            </div>
+        </body>
+    </html>
+    """;
+
+    [Fact]
+    public void GivenValidModelWhenMappingThenMapSuccessfully()
+    {
+        var result = HtmlMapper.Map<SearchByClassModel>(GivenHtml);
+
+        Assert.Multiple(
+            () => result.MainContent.Trim().Should().Be("Lorem ipsum"),
+            () => result.Footer.Trim().Should().Be("Bottom line")
+        );
+    }
+}
+
+internal sealed class ByClassAttribute : AtAttribute
+{
+    public ByClassAttribute(string className)
+        : base($".//*[@class='{className}']") { }
+}
+
+[At("//body")]
+internal sealed record SearchByClassModel(
+    [ByClass("main")] string MainContent,
+    [ByClass("footer")] string Footer
+);
