52b1de5c135583796e5d7bc014ae0ce153d6a329
Kacper Tonia <kacpert314@gmail.com>
2023-02-22 01:55:06 +0100

    Add sad path test

---
+++ tests/XMaps.UnitTests/CustomAtAttribute/SadPath/MultipleAttributesForOnePropertyTests.cs
@@ -0,0 +1,33 @@
+﻿using XMaps.Exceptions;
+using XMaps.UnitTests.CustomAtAttribute.HappyPath;
+
+namespace XMaps.UnitTests.CustomAtAttribute.SadPath;
+
+public class MultipleAttributesForOnePropertyTests
+{
+    private const string GivenHtml = """
+    <html>
+        <body>
+            <div class="main">
+                <h1>Header</h1>
+            </div>
+        </body>
+    </html>
+    """;
+
+    [Fact]
+    public void GivenMultipleAttributesForOnePropertyWhenMappingThenThrowModelDefinitionException()
+    {
+        var act = () => HtmlMapper.Map<MultipleAttributesForOnePropertyModel>(GivenHtml);
+
+        act.Should().ThrowExactly<ModelDefinitionException>()
+            .WithMessage("Parameter 'Header' contains 2 [At] attributes; exactly 1 is expected.");
+    }
+}
+
+[At("//body")]
+record MultipleAttributesForOnePropertyModel(
+    [At("./div[@class='main']")]
+    [ByClass("h1")]
+    string Header
+);
